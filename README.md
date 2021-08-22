38dfdc09fa79b76d93bbe3867871134b4e3d8122
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-08-22 16:52:27 -0700

    variable and color outline info-description

--- README.md
+++ README.md
@@ -18,12 +18,7 @@ The following images demonstrates the application functionality:
 
 
 
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
 
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
 
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,55 @@
 
+//Calendar date information
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY, h:mm "));
+
+//variable per time slot
+var btnNine = $('#hour-9');
+var btnTen = $('#hour-10');
+var btnEleven = $('#hour-11');
+var btnTwelve= $('#hour-12');
+var btnOne = $('#hour-1');
+var btnTwo = $('#hour-2');
+var btnThree = $('#hour-3');
+var btnFour = $('#hour-4');
+var btnFive = $('#hour-5');
+var currentHour = moment().format("HH");
+console.log(currentHour);
 
 
+// outline color of time slots as web page opens
+var textInfo9 = $('#text-9');
+var textInfo10 = $('#text-10');
+var textInfo11 = $('#text-11');
+var textInfo12 = $('#text-12');
+var textInfo1 = $('#text-1');
+var textInfo2 = $('#text-2');
+var textInfo3 = $('#text-3');
+var textInfo4 = $('#text-4');
+var textInfo5 = $('#text-5');
+
+var btnAll = $(".saveBtn");
+
+
+
+
+
+
+
+
+
+
+btnNine.on('click', function() {
+
+} )
+// assign past time slots to the grey color
+
+// assign present time slots the red color
+
+// assign future time slots the gree color 
+
+// save button 
+
+// save button saves scheduled task
+
+// the task is saved onto the page even after refreshing the web page
